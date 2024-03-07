0c841669f6f39f7f85a98c575e7997bc1e77c7f9
Sofía Mora Muñoz <sofiamoramunoz6@gmail.com>
2024-03-06 22:23:53 -0600

    Add angular and bootstrap
    npm install @angular/material
    npm install bootstrap
    
    Correr el siguiente comando antes de correr la aplicación:
    npm install
    
    Luego correr la aplicación: ng serve

---
+++ ACCH-app/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [
+    // Componentes de tu aplicación
+  ],
+  imports: [
+    CommonModule,
+    MatButtonModule
+  ],
+  providers: []
+})
+export class AppModule { }
