f8c9baf71ff6548693429d0f31d3710e44b092c9
Sofía Mora Muñoz <sofiamoramunoz6@gmail.com>
2024-06-13 00:58:57 -0600

    Changes from Yari

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { environment } from '../environments/environment';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
@@ -10,9 +11,6 @@ import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 
 import { AngularFireModule } from '@angular/fire/compat';
 import { StorageModule } from '@angular/fire/storage';
-import { AppComponent } from './app.component';
-import { PasarelaPagosComponent } from './components/pasarela-pagos/pasarela-pagos.component';
-import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
@@ -20,6 +18,7 @@ import { HeaderComponent } from './components/header/header.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     StorageModule,
     AngularFireModule.initializeApp(environment.firebase),
     provideAuth(() => getAuth())
