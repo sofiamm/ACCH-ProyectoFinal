acfaf374e37488d1cfc2f0166c53e67352449d17
Sofía Mora Muñoz <sofiamoramunoz6@gmail.com>
2024-06-13 16:36:16 -0600

    Merged Meli

--- proyecto/src/app/components/pasarela-pagos/pasarela-pagos.component.ts
+++ proyecto/src/app/components/pasarela-pagos/pasarela-pagos.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIcon } from '@angular/material/icon';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pasarela-pagos',
@@ -14,6 +15,14 @@ import { MatIcon } from '@angular/material/icon';
   templateUrl: './pasarela-pagos.component.html',
   styleUrl: './pasarela-pagos.component.scss'
 })
-export class PasarelaPagosComponent {
+export class PasarelaPagosComponent implements OnInit{
+  cursoSeleccionado: String = '';
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.cursoSeleccionado = params['curso'];
+    });
+  }
 }
