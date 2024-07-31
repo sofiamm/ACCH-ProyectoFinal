285ec96b04a45d1c4dca244ce81248211af68f46
Sofía Mora Muñoz <sofiamoramunoz6@gmail.com>
2024-07-31 16:31:30 -0600

    General changes

--- acch-proyectofinal/src/app/models/curso.model.ts
+++ acch-proyectofinal/src/app/models/curso.model.ts
@@ -1,11 +1,12 @@
 import { Usuario } from "./usuario.model";
+import { Video } from "./video.model";
 
 export interface Curso {
-    id?: string;
-    nombre: string;
-    descripcion: string;
-    evaluacion: string;
-    imagen: string;
-    instructor?: Usuario;
-  }
-  
+  id?: string;
+  nombre: string;
+  descripcion: string;
+  evaluacion: string;
+  imagen: string;
+  instructor?: Usuario;
+  videos?: Video[];
+}
