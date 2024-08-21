9cf89b95b382e27a0abbcf02012731b5eacebee6
MelissaRamirezG <mramirez30217@ufide.ac.cr>
2024-08-21 17:23:05 -0600

    agrego servicios, cursos y recibo
    Se agregaron servicios, cursos y recibo.

---
+++ acch-proyectofinal/src/app/models/recibo.model.ts
@@ -0,0 +1,11 @@
+export interface Recibo {
+    id: string;
+    usrNombre: string;
+    usrCorreo: string;
+    usrTelefono: string;
+    curso: string;
+    material: string;
+    comprobanteImg: string;
+    monto: string;
+    estado: string;
+}
