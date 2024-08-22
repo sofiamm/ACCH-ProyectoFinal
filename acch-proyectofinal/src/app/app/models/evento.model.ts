73c574d236787ccd1e8aa56355dd903c001a9b8a
jim29-icu <jim29sozasancho@gmail.com>
2024-08-21 19:51:40 -0600

    depuracion de carpetas
    depuracion de carpetas

--- acch-proyectofinal/src/app/app/models/evento.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Evento {
-    nombre: string;
-    profesor: string;
-    descripcion: string;
-    fecha: string; // Puedes cambiar el tipo a Date si prefieres trabajar con objetos Date
-    hora: string;
-  }
-  
